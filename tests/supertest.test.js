816279ada4dcd980ea2d27c5a0839d30015ee9c1
✨ William Maldonado ✨ <willpaiolo@live.com>
2024-01-06 12:09:19 -0600

    add comments to supertest file

--- tests/supertest.test.js
+++ tests/supertest.test.js
@@ -1,9 +1,10 @@
-const request = require ("supertest");
+const request = require ("supertest"); // allows test HTTP requests
 const app = require ("../index.js");
-const http = require ("http");
+const http = require ("http"); // module allow to create server
 
 let server;
 
+// create server, start listening
 beforeAll(() => {
     server = http.createServer(app);
     server.listen();
@@ -13,6 +14,7 @@ afterAll((done) => {
     server.close(done);
 });
 
+// Define test for GET / route
 test("GET /", async () => {
     const response = await request(server).get("/");
     expect(response.text).toBe("✨ Hello DevOps Demo! CI Completed ✨");
