33761277b8b591ca4403fee945dfb08640f11f63
✨ William Maldonado ✨ <willpaiolo@live.com>
2024-01-05 23:47:37 -0600

    add top level await to the import fuction of vitest

--- tests/supertest.test.js
+++ tests/supertest.test.js
@@ -1,13 +1,36 @@
-import { expect, test } from "vitest";
-import request from "supertest";
-import app from "../index";
+// import { expect, test } from "vitest";
+// import request from "supertest";
+// import app from "../index";
 
-let server //store an instance of your server
+// const { expect, test } = require("vitest");
+// const request = require("supertest");
+// const app = require("../index"); 
 
-test("GET /", async () => {
-    server = app.listen()
-    await request(server)
-        .get("/")
-        .expect("✨ Hello DevOps Demo! ✨")
-    server.close()    
-})
+// let server //store an instance of your server
+
+// test("GET /", async () => {
+//     server = app.listen()
+//     await request(server)
+//         .get("/")
+//         .expect("✨ Hello DevOps Demo! ✨")
+//     server.close()    
+// })
+
+// -----------------------
+
+const request = require('supertest');
+import app from '../index.js';
+
+let server; //store an instance of your server
+
+// Use dynamic import() syntax to import the vitest module
+const { expect, test } = await import('vitest');
+
+test('GET /', async () => {
+server = app.listen();
+await request(server)
+    .get('/')
+    .expect('✨ Hello DevOps Demo! ✨');
+server.close();
+});
+
